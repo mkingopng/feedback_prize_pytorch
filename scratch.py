0f5c523a713cef5b30847a33bb2c8e579b0360fd
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-02-26 20:43:55 +1000

    trying again...

---
+++ scratch.py
@@ -0,0 +1,327 @@
+"""
+https://www.kaggle.com/cdeotte/pytorch-bigbird-ner-cv-0-615
+
+this seems like a good place ot restart. He's using an NER.csv to augment
+"""
+# imports
+import os
+import numpy as np
+import pandas as pd
+import gc
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+
+from transformers import *
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch import cuda
+
+# declare how many gpus you wish to use. kaggle only has 1, but offline, you can use more
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # 0,1,2,3 for four gpu
+
+# version for saving model weights
+VER = 26
+
+# if variable is none, then notebook computes tokens otherwise notebook loads tokens from path
+LOAD_TOKENS_FROM = 'py-bigbird-v26'
+
+# if variable is none, then notebook trains a new model otherwise it loads your previously trained model
+LOAD_MODEL_FROM = 'py-bigbird-v26'
+
+# if following is none, then notebook uses internet and downloads huggingface config, tokenizer, and model
+DOWNLOADED_MODEL_PATH = 'py-bigbird-v26'
+
+if DOWNLOADED_MODEL_PATH is None:
+    DOWNLOADED_MODEL_PATH = 'model'
+MODEL_NAME = 'google/bigbird-roberta-base'
+
+config = {'model_name': MODEL_NAME,
+          'max_length': 1024,
+          'train_batch_size': 4,
+          'valid_batch_size': 4,
+          'epochs': 5,
+          'learning_rates': [2.5e-5, 2.5e-5, 2.5e-6, 2.5e-6, 2.5e-7],
+          'max_grad_norm': 10,
+          'device': 'cuda' if cuda.is_available() else 'cpu'}
+
+# THIS WILL COMPUTE VAL SCORE DURING COMMIT BUT NOT DURING SUBMIT
+COMPUTE_VAL_SCORE = True
+if len(os.listdir('data/test')) > 5:
+    COMPUTE_VAL_SCORE = False
+
+if DOWNLOADED_MODEL_PATH == 'model':
+    os.mkdir('model')
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, add_prefix_space=True)
+    tokenizer.save_pretrained('model')
+
+    config_model = AutoConfig.from_pretrained(MODEL_NAME)
+    config_model.num_labels = 15
+    config_model.save_pretrained('model')
+
+    backbone = AutoModelForTokenClassification.from_pretrained(MODEL_NAME, config=config_model)
+    backbone.save_pretrained('model')
+
+train_df = pd.read_csv('data/train.csv')
+print(train_df.shape)
+train_df.head()
+
+# https://www.kaggle.com/raghavendrakotala/fine-tunned-on-roberta-base-as-ner-problem-0-533
+test_names, test_texts = [], []
+for f in list(os.listdir('data/test')):
+    test_names.append(f.replace('.txt', ''))
+    test_texts.append(open('data/test/' + f, 'r').read())
+test_texts = pd.DataFrame({'id': test_names, 'text': test_texts})
+print(test_texts.head())
+
+# https://www.kaggle.com/raghavendrakotala/fine-tunned-on-roberta-base-as-ner-problem-0-533
+test_names, train_texts = [], []
+for f in tqdm(list(os.listdir('data/train'))):
+    test_names.append(f.replace('.txt', ''))
+    train_texts.append(open('data/train/' + f, 'r').read())
+train_text_df = pd.DataFrame({'id': test_names, 'text': train_texts})
+print(train_text_df.head())
+
+if not LOAD_TOKENS_FROM:
+    all_entities = []
+    for ii, i in enumerate(train_text_df.iterrows()):
+        if ii % 100 == 0: print(ii, ', ', end='')
+        total = i[1]['text'].split().__len__()
+        entities = ["O"] * total
+        for j in train_df[train_df['id'] == i[1]['id']].iterrows():
+            discourse = j[1]['discourse_type']
+            list_ix = [int(x) for x in j[1]['predictionstring'].split(' ')]
+            entities[list_ix[0]] = f"B-{discourse}"
+            for k in list_ix[1:]: entities[k] = f"I-{discourse}"
+        all_entities.append(entities)
+    train_text_df['entities'] = all_entities
+    train_text_df.to_csv('train_NER.csv', index=False)
+
+else:
+    from ast import literal_eval
+
+    train_text_df = pd.read_csv(f'{LOAD_TOKENS_FROM}/train_NER.csv')
+    # pandas saves lists as string, we must convert back
+    train_text_df.entities = train_text_df.entities.apply(lambda x: literal_eval(x))
+
+print(train_text_df.shape)
+print(train_text_df.head())
+
+# create dictionaries that we can use during train and infer # mk: these are the entities
+output_labels = ['O', 'B-Lead', 'I-Lead', 'B-Position', 'I-Position', 'B-Claim', 'I-Claim', 'B-Counterclaim',
+                 'I-Counterclaim', 'B-Rebuttal', 'I-Rebuttal', 'B-Evidence', 'I-Evidence', 'B-Concluding Statement',
+                 'I-Concluding Statement']
+
+labels_to_ids = {v: k for k, v in enumerate(output_labels)}
+ids_to_labels = {k: v for k, v in enumerate(output_labels)}
+
+print(labels_to_ids)
+
+# define the dataset function
+LABEL_ALL_SUBTOKENS = True  # mk: this is important
+
+
+class Dataset(Dataset):
+    def __init__(self, dataframe, tokenizer, max_len, get_wids):
+        self.len = len(dataframe)
+        self.data = dataframe
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.get_wids = get_wids  # for validation
+
+    def __getitem__(self, index):
+        # get text and word labels
+        text = self.data.text[index]
+        word_labels = self.data.entities[index] if not self.get_wids else None
+
+        # tokenize text
+        encoding = self.tokenizer(text.split(),
+                                  is_split_into_words=True,
+                                  # return_offsets_mapping=True,
+                                  padding='max_length',
+                                  truncation=True,
+                                  max_length=self.max_len)
+        word_ids = encoding.word_ids()
+
+        # create targets
+        if not self.get_wids:
+            previous_word_idx = None
+            label_ids = []
+            for word_idx in word_ids:
+                if word_idx is None:
+                    label_ids.append(-100)
+                elif word_idx != previous_word_idx:
+                    label_ids.append(labels_to_ids[word_labels[word_idx]])
+                else:
+                    if LABEL_ALL_SUBTOKENS:
+                        label_ids.append(labels_to_ids[word_labels[word_idx]])
+                    else:
+                        label_ids.append(-100)
+                previous_word_idx = word_idx
+            encoding['labels'] = label_ids
+
+        # convert to torch tensors
+        item = {key: torch.as_tensor(val) for key, val in encoding.items()}
+        if self.get_wids:
+            word_ids2 = [w if w is not None else -1 for w in word_ids]
+            item['wids'] = torch.as_tensor(word_ids2)
+
+        return item
+
+    def __len__(self):
+        return self.len
+
+
+# create train and validation data loaders
+# choose validation indexes (that match my TF notebook)
+IDS = train_df.id.unique()
+print('There are', len(IDS), 'train texts. We will split 90% 10% for validation.')
+
+# train valid split 90% 10%
+np.random.seed(42)
+train_idx = np.random.choice(np.arange(len(IDS)), int(0.9 * len(IDS)), replace=False)
+valid_idx = np.setdiff1d(np.arange(len(IDS)), train_idx)
+np.random.seed(None)
+
+# create train subset and valid subset
+data = train_text_df[['id', 'text', 'entities']]  # mk: this is where the NER csv gets used
+train_dataset = data.loc[data['id'].isin(IDS[train_idx]), ['text', 'entities']].reset_index(drop=True)  # mk: one part of the NER.csv is used for train_dataset
+test_dataset = data.loc[data['id'].isin(IDS[valid_idx])].reset_index(drop=True)  # mk: the other part is used for the test dataset
+
+print("FULL Dataset: {}".format(data.shape))
+print("TRAIN Dataset: {}".format(train_dataset.shape))
+print("TEST Dataset: {}".format(test_dataset.shape))
+
+tokenizer = AutoTokenizer.from_pretrained(DOWNLOADED_MODEL_PATH)  # mk: this is using the pretrained tokenizer
+training_set = Dataset(train_dataset, tokenizer, config['max_length'], False)
+testing_set = Dataset(test_dataset, tokenizer, config['max_length'], True)
+
+# train dataset and valid dataset
+train_params = {'batch_size': config['train_batch_size'],
+                'shuffle': True,
+                'num_workers': 2,
+                'pin_memory': True
+                }
+
+test_params = {'batch_size': config['valid_batch_size'],
+               'shuffle': False,
+               'num_workers': 2,
+               'pin_memory': True
+               }
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+
+# test dataset
+test_texts_set = Dataset(test_texts, tokenizer, config['max_length'], True)
+test_texts_loader = DataLoader(test_texts_set, **test_params)
+
+
+# https://www.kaggle.com/raghavendrakotala/fine-tunned-on-roberta-base-as-ner-problem-0-533
+def train(epoch):
+    tr_loss, tr_accuracy = 0, 0
+    nb_tr_examples, nb_tr_steps = 0, 0
+    # tr_preds, tr_labels = [], []
+
+    # put model in training mode
+    model.train()
+
+    for idx, batch in enumerate(training_loader):
+
+        ids = batch['input_ids'].to(config['device'], dtype=torch.long)
+        mask = batch['attention_mask'].to(config['device'], dtype=torch.long)
+        labels = batch['labels'].to(config['device'], dtype=torch.long)
+
+        loss, tr_logits = model(input_ids=ids, attention_mask=mask, labels=labels, return_dict=False)
+        tr_loss += loss.item()
+
+        nb_tr_steps += 1
+        nb_tr_examples += labels.size(0)
+
+        if idx % 200 == 0:
+            loss_step = tr_loss / nb_tr_steps
+            print(f"Training loss after {idx:04d} training steps: {loss_step}")
+
+        # compute training accuracy
+        flattened_targets = labels.view(-1)  # shape (batch_size * seq_len,)
+        active_logits = tr_logits.view(-1, model.num_labels)  # shape (batch_size * seq_len, num_labels)
+        flattened_predictions = torch.argmax(active_logits, axis=1)  # shape (batch_size * seq_len,)
+
+        # only compute accuracy at active labels
+        active_accuracy = labels.view(-1) != -100  # shape (batch_size, seq_len)
+        # active_labels = torch.where(active_accuracy, labels.view(-1), torch.tensor(-100).type_as(labels))
+
+        labels = torch.masked_select(flattened_targets, active_accuracy)
+        predictions = torch.masked_select(flattened_predictions, active_accuracy)
+
+        # tr_labels.extend(labels)
+        # tr_preds.extend(predictions)
+
+        tmp_tr_accuracy = accuracy_score(labels.cpu().numpy(), predictions.cpu().numpy())
+        tr_accuracy += tmp_tr_accuracy
+
+        # gradient clipping
+        torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=config['max_grad_norm'])
+
+        # backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    epoch_loss = tr_loss / nb_tr_steps
+    tr_accuracy = tr_accuracy / nb_tr_steps
+    print(f"Training loss epoch: {epoch_loss}")
+    print(f"Training accuracy epoch: {tr_accuracy}")
+
+
+# create model
+config_model = AutoConfig.from_pretrained(DOWNLOADED_MODEL_PATH + '/config.json')
+model = AutoModelForTokenClassification.from_pretrained(
+    DOWNLOADED_MODEL_PATH + '/pytorch_model.bin', config=config_model)
+model.to(config['device'])
+optimizer = torch.optim.Adam(params=model.parameters(), lr=config['learning_rates'][0])
+
+# loop to train model (or load model)
+if not LOAD_MODEL_FROM:
+    for epoch in range(config['epochs']):
+        print(f"### Training epoch: {epoch + 1}")
+        for g in optimizer.param_groups:
+            g['lr'] = config['learning_rates'][epoch]
+        lr = optimizer.param_groups[0]['lr']
+        print(f'### LR = {lr}\n')
+        train(epoch)
+        torch.cuda.empty_cache()
+        gc.collect()
+    torch.save(model.state_dict(), f'bigbird_v{VER}.pt')
+else:
+    model.load_state_dict(torch.load(f'{LOAD_MODEL_FROM}/bigbird_v{VER}.pt'))
+    print('Model loaded.')
+
+
+# inference and validation loop
+def inference(batch):
+    # move batch to gpu and infer
+    ids = batch["input_ids"].to(config['device'])
+    mask = batch["attention_mask"].to(config['device'])
+    outputs = model(ids, attention_mask=mask, return_dict=False)
+    all_preds = torch.argmax(outputs[0], axis=-1).cpu().numpy()
+
+    # iterate through each text and get pred
+    predictions = []
+    for k, text_preds in enumerate(all_preds):
+        token_preds = [ids_to_labels[i] for i in text_preds]
+
+        prediction = []
+        word_ids = batch['wids'][k].numpy()
+        previous_word_idx = -1
+        for idx, word_idx in enumerate(word_ids):
+            if word_idx == -1:
+                pass
+            elif word_idx != previous_word_idx:
+                prediction.append(token_preds[idx])
+                previous_word_idx = word_idx
+        predictions.append(prediction)
+
+    return predictions
