db3350320ded09f44bbbc018e94e92b79dc5a08e
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-03-03 22:21:25 +1000

    tidying up to try to keep track of multiple approaches

--- at_approach/at_config.py
+++ at_approach/at_config.py
@@ -91,7 +91,7 @@ class Targets:
 
 
 class Args1:
-    input_path = "data"
+    input_path = "../data"
     model = "longformer"  # change this to match the model used for training
     tez_model = "fblongformerlarge1536"  # change this to one of the other trained models to test
     output = "."
@@ -100,7 +100,7 @@ class Args1:
 
 
 class Args2:
-    input_path = "data"
+    input_path = "../data"
     model = "longformer-large-4056"  # change this to match the model used for training
     tez_model = "tez-fb-large"  # change this to one of the other trained models to test
     output = "."
